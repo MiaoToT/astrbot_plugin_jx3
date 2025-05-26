099a290b25d17b8efc79f41923ffd0799ead98c1
MiaoToT <miaotoot@outlook.com>
2025-05-26 22:41:59 +0800

    feat(master): 定时任务允许传入方法参数
    [skip ci]

--- util/job_util.py
+++ util/job_util.py
@@ -13,15 +13,17 @@ class CronSchedulerUtil:
         self.tasks = []  # 任务列表
         self.task_handles = []  # 任务句柄存储(启动的任务)
 
-    def add_task(self, job_func, cron_expr) -> None:
+    def add_task(self, job_func, cron_expr, *args, **kwargs) -> None:
         """添加定时任务
 
         Args:
             job_func: 函数名
             cron_expr: cron 表达式
+            *args: 方法参数
+            **kwargs: 方法关键字参数
         """
-        self.tasks.append((job_func, cron_expr))
-        self._start_single_task(job_func, cron_expr)
+        self.tasks.append((job_func, cron_expr, args, kwargs))
+        self._start_single_task(job_func, cron_expr, *args, **kwargs)
 
     async def stop(self) -> None:
         """停止所有定时任务"""
@@ -31,23 +33,27 @@ class CronSchedulerUtil:
         await asyncio.gather(*self.task_handles, return_exceptions=True)
         self.task_handles.clear()
 
-    def _start_single_task(self, job_func, cron_expr) -> None:
+    def _start_single_task(self, job_func, cron_expr, *args, **kwargs) -> None:
         """启动单个任务调度器
 
         Args:
             job_func: 函数名
             cron_expr: cron 表达式
+            *args: 方法参数
+            **kwargs: 方法关键字参数
         """
-        handle = asyncio.create_task(self._cron_worker(job_func, cron_expr))
+        handle = asyncio.create_task(self._cron_worker(job_func, cron_expr, *args, **kwargs))
         self.task_handles.append(handle)
 
     @staticmethod
-    async def _cron_worker(job_func, cron_expr) -> None:
+    async def _cron_worker(job_func, cron_expr, *args, **kwargs) -> None:
         """实际执行调度的协程
 
         Args:
             job_func: 函数名
             cron_expr: cron 表达式
+            *args: 方法参数
+            **kwargs: 方法关键字参数
         """
         cron = croniter(cron_expr, datetime.now())
 
@@ -60,7 +66,7 @@ class CronSchedulerUtil:
                 # 精准等待
                 if wait_seconds > 0:
                     await asyncio.sleep(wait_seconds)
-                asyncio.create_task(job_func())  # 执行任务（并发执行）
+                asyncio.create_task(job_func(*args, **kwargs))  # 执行任务（并发执行）
             except asyncio.CancelledError:
                 break  # 任务被取消时退出
             except Exception as e:
